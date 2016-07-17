68cedcf4db55a21815980a5bb11c0c433403aa61
Smily <mailToSmily@gmail.com>
2016-07-17 22:41:35 +0300

    Homework BSA2016 Task 7 Tooling + Modelarity

---
+++ N7. Tooling + Modularity/gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var rjs = require('gulp-requirejs');
+var uglify = require('gulp-uglify');
+
+gulp.task('connect', function() {
+	connect.server({
+		port: 47,
+		livereload: true,
+		root: ['dist', 'dist/html']
+	});
+});
+
+gulp.task('jade', function() {
+	gulp.src('src/jade/*.jade')
+		.pipe(jade())
+		.pipe(gulp.dest('dist/html'))
+		.pipe(connect.reload());
+});
+
+gulp.task('sass', function() {
+	return gulp.src('src/sass/*.sass')
+	.pipe(sass.sync().on('error', sass.logError))
+	.pipe(gulp.dest('dist/css/'))
+	.pipe(connect.reload());
+});
+
+gulp.task('requireJS', function() {
+	rjs({
+		baseUrl: 'src/js',
+		name: '../../node_modules/almond/almond',
+		include: ['app'],
+		insertRequire: ['app'],
+		out: 'bundle.js',
+		wrap: true
+	})
+	.pipe(gulp.dest('dist/js'))
+	.pipe(connect.reload());
+});
+
+gulp.task('minify', function () {
+	 gulp.src('dist/js/bundle.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js/uglify/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/jade/*.jade', ['jade']);
+	gulp.watch('src/sass/*.sass', ['sass']);
+	gulp.watch('src/js/*.js', ['requireJS']);
+});
+
+gulp.task('default', ['requireJS', 'minify', 'jade', 'sass', 'connect', 'watch']);
